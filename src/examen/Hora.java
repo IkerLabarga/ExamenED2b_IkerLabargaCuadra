4587febf921731742437c0b47d37386a20c28758
IkerLabarga <ikerlabargacuadra@gmal.com>
2023-03-31 09:35:00 +0200

    Proyecto documentado

--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,8 +1,27 @@
 package examen;
 
-public class Hora {
+/**
+ *
+ * @author dam107
+ * @version 1.0
+ * 
+ */
 
+public class Hora {
+	/**
+	 * @param horaValida Booleano para validar la fecha
+	 */
 	public static boolean horaValida;
+	
+	/**
+	 * Metodo que devuelve si una hora del dia es válida o no (Boolean)
+	 * @param segundos Segundos de el minuto (int)
+	 * @param minutos Minutos de la hora (int)
+	 * @param horas Hora del día (int)
+	 * @return Devuelve el parametro horaValida que dice si la hora es válida(True)
+	 *         o no válida(False).
+	 */
+	
 	public static boolean validarHora(int segundos, int minutos, int horas) {
 		horaValida = true;
 		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
@@ -18,5 +37,5 @@ public class Hora {
 		}
 		return horaValida;
 	}
-	
+
 }
